34232888742b25c5a99c03d9e0536caffcf8eaea
0010beck <reacone@outlook.com>
2023-01-10 14:54:45 -0500

    Initial commit

---
+++ Exercise 1/template-p5-project/js/script.js
@@ -0,0 +1,27 @@
+/**
+Rebecca Acone
+Test
+*/
+
+"use strict";
+
+
+
+function preload() {
+  // mySound = loadSound('assets/sounds/bark.wav');
+  // reverb = new p5.Reverb();
+}
+
+//sets up canvas
+
+function setup(){
+ createCanvas(500, 500);
+ background(0,255,0);
+}
+
+
+
+function draw() {
+}
+
+
